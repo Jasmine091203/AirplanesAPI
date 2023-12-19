d37b592b37f7211241ffb2143f6ecc3a1e273f0f
Jasmine091203 <149470955+Jasmine091203@users.noreply.github.com>
2023-12-19 16:47:17 +0800

    upload

---
+++ Airplanes/Models/Airplane.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Airplanes.Models
+{
+    public class Airplane
+    {
+        [Key] public Guid Pid { get; set; }
+        [Required] public String Pname { get; set; }
+        [Required] public int Pseats { get; set; }
+        [Required] public int Pmaxspeed { get; set; }
+        public float Pheavyload { get; set; }
+    }
+}
